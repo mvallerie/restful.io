ff1a542e9f5a43538b856ec1c5c98da822a6bec6
Mikael Vallerie <mikael.vallerie@gmail.com>
2014-08-14 13:17:25 +0000

    First example : usercrud

---
+++ examples/usercrud/client.js
@@ -0,0 +1,30 @@
+var io = require('socket.io-client');
+
+var socket = io('http://localhost:8080');
+socket.on('connect', function() {
+  socket.emit('PUT', {
+    uri: '/user/j:user',
+    json: {
+      user: {
+        name: "John Smith",
+        age: 42
+      }
+    }
+  });
+
+  socket.on('PUT:RESULT', function(data) {
+    console.log("PUT RESULT : " + JSON.stringify(data));
+
+    socket.emit('GET', '/user/'+data.id);
+
+    socket.on('GET:RESULT', function(data) {
+      console.log("GET RESULT : " + JSON.stringify(data));
+      socket.emit('DELETE', '/user/'+data.id);
+
+      socket.on('DELETE:RESULT', function(data) {
+        console.log("DELETE RESULT : " + JSON.stringify(data));
+      });
+    });
+
+  });
+});
---
+++ examples/usercrud/index.js
@@ -0,0 +1,81 @@
+var RestfulRouter = require("../../index");
+var http = require('http').Server();
+var io = require("socket.io")(http);
+var _ = require("underscore");
+
+var log = function(str) {
+  console.log("[LOG] usercrud example: " + str);
+};
+
+var UserId = 0;
+var UserStore = [];
+
+var UserController = {
+  get: function(id) {
+    log("UserController received request GET for " + id);
+    return _.filter(UserStore, function(e) {
+      return e.id == id;
+    })[0];
+  },
+  put: function(data) {
+    var user = data;
+    if(user.id !== undefined) {
+      log("UserController received request PUT for " + user.name + " (" + user.id + ")");
+      var stored = _.filter(UserStore, function(e) {
+        return e.id == user.id;
+      });
+      if(stored.length == 1) {
+        stored[0].name = user.name;
+        stored[0].age = user.age;
+      }
+    } else {
+      log("UserController received request PUT for " + user.name + " (new)");
+      user.id = UserId;
+      UserStore.push(user);
+      UserId = UserId + 1;
+    }
+    return user;
+  },
+  delete: function(id) {
+    log("UserController received request DELETE for " + id);
+    var before = UserStore.length;
+    UserStore = _.filter(UserStore, function(e) { return e.id != id; });
+    return {deleted: before - UserStore.length};
+  }
+};
+
+
+
+
+
+// Here we go with the router
+
+var ControllerScope = {
+  "UserController": UserController
+};
+
+var router = new RestfulRouter(ControllerScope, {
+  GET: [
+    {
+      uri: "/user/p:id",
+      to: "UserController.get(id)"
+    }
+  ],
+  PUT: [
+    {
+      uri: "/user/j:data",
+      to: "UserController.put(data)"
+    }
+  ],
+  DELETE: [
+    {
+      uri: "/user/p:id",
+      to: "UserController.delete(id)"
+    }
+  ]
+}, true);
+
+router.start(io);
+
+// Connections will be established on port 8080
+http.listen(8080);
